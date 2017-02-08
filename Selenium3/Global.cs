cb0343f6e65ca936f3269252837a92f62ddafa7b
ibenPak <ivan.nguyencuu143@gmail.com>
2017-02-08 15:35:04 -0800

    Added Global Variable Class

---
+++ Selenium3/Global.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+
+namespace Selenium3
+{
+    class Global
+    {
+
+        internal class Vars
+        {
+            public static string MMddyyMinus1 = DateTime.Today.AddMonths(-1).ToString("MM-dd-yy");
+
+            public static string MMddyyDate = DateTime.Today.ToString("MM-dd-yy");
+
+            public static string StreetNum = DateTime.Today.Date.ToString("MMdd").TrimStart('0');
+
+            public static string LoanNum = DateTime.Now.ToString("MMddyyHHmm").TrimStart('0');
+
+            public static string UniqueNum = DateTime.Now.ToString("ddHHmmss").TrimStart('0');
+
+
+        }
+
+
+    }
+
+
+}
+
+
+
